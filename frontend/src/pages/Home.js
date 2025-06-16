5b1257b50081ed3c4dfa9a64f68fc37bc4b6cdad
mbayajames <jmuthiora244@gmail.com>
2025-06-16 14:25:54 -0700

    Update Home.js layout for improved visual hierarchy

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>
+        <i className="fas fa-search"></i> Welcome to TafutaKazi
+      </h1>
+      <p>Find work or hire talent in your region.</p>
+    </div>
+  );
+};
+
+export default Home;
